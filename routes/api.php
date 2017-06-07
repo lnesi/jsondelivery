35c2ea4df196bc0497c173b1ab46a5a24b70f1e0
Luis Nesi <lenesi@hotmail.com>
2017-06-07 17:15:42 +0100

    Delivery API

--- routes/api.php
+++ routes/api.php
@@ -17,20 +17,60 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/delivery/{id}/{jsonp?}/{debug?}",function(Request $request,$id,$jsonp="jdeliveryCallback",$debug=false){
-	
+Route::get("/delivery/{id}/{content_id?}/{jsonp?}/{debug?}",function(Request $request,$id,$content_id=null,$jsonp="jdeliveryCallback",$debug=false){
 	if($debug && $debug==="debug"){
 		$debug=true;
 	}else{
 		$debug=false;
 	}
+
+	$item=App\Delivery::find($id);
+
 	
-	$item=App\Delivery::findOrFail($id);
-	if($item->status->id==2 || $debug){
-		$result=$item->getObject();
-		return response()->json($result)->withCallback($jsonp);;
-	}else{
-		return response()->json([ 'error'=> 404, 'message'=> 'Not found' ])->withCallback($jsonp);
+	if($item && ($item->status->id==2 || $debug)){
+		if(!$content_id){
+			if(!$debug){
+				$contents=$item->contents->where('status_id',2);
+			}else{
+				$contents=$item->contents;
+			}
+			$contentPull=[];
+			$totalW=1;
+			foreach($contents as $c){
+				$contentPull[]=["min"=>$totalW,"max"=>$totalW+$c->distribution-1,"dist"=>$c->distribution,"id"=>$c->id];
+				$totalW+=$c->distribution;
+			}
+			$contentPull=collect($contentPull);
+			$weightIndex=rand(1,$contents->sum('distribution'));
+
+			$contentSelect=$contentPull->where('min','<',$weightIndex)->where('max','>',$weightIndex)->first();
+			$content=$contents->where('id',$contentSelect['id'])->first();
+		}else{
+			if(!$debug){
+				$contents=$item->contents->where('status_id',2);
+			}else{
+				$contents=$item->contents;
+			}
+			$content=$contents->where('id',$content_id)->first();
+		}
+		
+		if($content){
+			$result=$item->getObject();
+		
+			$result->content=[];
+			foreach($item->customs as $custom){
+				$value=$content->values->where('custom_id',$custom->id)->first();
+				if($value){
+					$result->content[$custom->key]=$value->data;
+				}else{
+					$result->content[$custom->key]=null;
+				}
+			}
+			return response()->json($result)->withCallback($jsonp);
+		}
+		
+		
 	}
+	return response()->json([ 'error'=> 404, 'message'=> 'Not found' ]);
 	
 });
